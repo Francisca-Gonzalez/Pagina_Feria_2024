798cf022f806bb1d4cdc937f18d0cb1ad1edf76a
Francisca-Gonzalez <75760416+Francisca-Gonzalez@users.noreply.github.com>
2024-09-10 21:53:44 -0300

    aaaa

--- pagina_feria_2024/src/pages/digitalskillapp.jsx
+++ pagina_feria_2024/src/pages/digitalskillapp.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Container, Typography, Box, Grid } from '@mui/material';
-import '../stylesheets/inicio.css';
-import logo from '../assets/Logo.svg';
+import { Container, Typography, Box } from '@mui/material';
+import '../stylesheets/digitalskillapp.css';
 
 export default function Inicio() {
     return (
         <>
         <Container maxWidth="xl" className="appweb" sx={{ mt: 10 }}>
-            <Typography variant="h2" gutterBottom>Digital Skill App</Typography>
-            <Typography variant="h5" gutterBottom>
-                DigitalSkillApp es una aplicación web diseñada para evaluar las competencias digitales de los ciudadanos de manera práctica y real, impulsado con inteligencia artificial.
-            </Typography>
-            <Box className="appweb__problem" sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-                <Typography>¿Qué hace DigitalSkillApp?</Typography>
+            <Box align="center" sx={{ mb: 4 }}>
+                <Typography variant="h2" gutterBottom>Digital Skill App</Typography>
+                <Typography variant="h5" gutterBottom>
+                    DigitalSkillApp es una aplicación web diseñada para evaluar las competencias digitales de los ciudadanos de manera práctica y real, impulsado con inteligencia artificial.
+                </Typography>
+            </Box>
+                
+            <Box align="center" className="appweb__problem" >
+                <Typography variant="h2" gutterBottom>¿Qué hace DigitalSkillApp?</Typography>
+                <Typography variant="h5" gutterBottom>
+                    Algo creo.
+                </Typography>
             </Box>
             
-        </Container>
-
-        
+        </Container>        
         </>
     );
 };
