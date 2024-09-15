d8f3172eeced2779ea7e7a1f0f5dacf0cd53e774
SebastianBaeza <sebastian.baezaa@usm.cl>
2024-09-14 21:39:29 -0300

    cambios menores

--- pagina_feria_2024/src/pages/Beneficios.jsx
+++ pagina_feria_2024/src/pages/Beneficios.jsx
@@ -2,32 +2,36 @@ import React from 'react';
 import { Grid2, Typography } from '@mui/material';
 const Beneficios = () => {
     return (
-    <Grid2 container className="homepage__grid" >
-        <Grid2 className="homepage__griditem" xs={1} md={3}>
-            <Typography className="homepage__text" variant="h6" gutterBottom>Pruebas Personalizados</Typography>
+        <>
+    <Grid2 container className="homepage__grid" sx={{mt: 8}}>
+        <Grid2 className="homepage__griditem" xs={1} md={4}>
+            <Typography className="homepage__subtitle">Pruebas Personalizados</Typography>
             <Typography className="homepage__text">Realiza pruebas adaptadas a tu nivel y habilidades.</Typography>
         </Grid2>
-        <Grid2 className="homepage__griditem" xs={1} md={3}>
-            <Typography className="homepage__text" variant="h6" gutterBottom>Actividades Prácticas</Typography>
+        <Grid2 className="homepage__griditem" xs={1} md={4}>
+            <Typography className="homepage__subtitle">Actividades Prácticas</Typography>
             <Typography className="homepage__text">Simulaciones reales que ponen tus habilidades digitales a prueba</Typography>
         </Grid2>
-        <Grid2 className="homepage__griditem" xs={1} md={3}>
-            <Typography className="homepage__text" variant="h6" gutterBottom>Basado en DigComp 2.2</Typography>
+        <Grid2 className="homepage__griditem" xs={1} md={4}>
+            <Typography className="homepage__subtitle">Basado en DigComp 2.2</Typography>
             <Typography className="homepage__text">Desarrolla competencias clave según estándares europeos.</Typography>
         </Grid2>
-        <Grid2 className="homepage__griditem" xs={1} md={3}>
-            <Typography className="homepage__text" variant="h6" gutterBottom>IA Generativa</Typography>
+    </Grid2>
+    <Grid2 container className="homepage__grid" sx={{mt: 4}}>
+        <Grid2 className="homepage__griditem" xs={1} md={4}>
+            <Typography className="homepage__subtitle">IA Generativa</Typography>
             <Typography className="homepage__text">Genera preguntas y analiza respuestas con IA avanzada.</Typography>
         </Grid2>
-        <Grid2 className="homepage__griditem" xs={1} md={3}>
-            <Typography className="homepage__text" variant="h6" gutterBottom>Secuencia y medición del tiempo</Typography>
-            <Typography className="homepage__text">Mide tu rendimiento mientras avanzas en pruebas secuenciales.</Typography>
+        <Grid2 className="homepage__griditem" xs={1} md={4}>
+            <Typography className="homepage__subtitle">Secuencia y medición del tiempo</Typography>
+            <Typography className="homepage__text">Mide tu rendimiento mientras sigues pruebas secuenciales.</Typography>
         </Grid2>
-        <Grid2 className="homepage__griditem" xs={1} md={3}>
-            <Typography className="homepage__text" variant="h6" gutterBottom>Feedback completo</Typography>
+        <Grid2 className="homepage__griditem" xs={1} md={4}>
+            <Typography className="homepage__subtitle">Feedback completo</Typography>
             <Typography className="homepage__text">Resultados detallados para evaluar tus competencias digitales al instante.</Typography>
         </Grid2>
     </Grid2>
+        </>
     );
 };
 
